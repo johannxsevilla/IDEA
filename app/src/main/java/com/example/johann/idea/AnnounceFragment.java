c49d9e84bf8215d761b7493f3181894b5c079a64
Johann <johannxsevilla@gmail.com>
2015-04-28 19:07:45 -0700

    Initial commit

---
+++ app/src/main/java/com/example/johann/idea/AnnounceFragment.java
@@ -0,0 +1,23 @@
+package com.example.johann.idea;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AnnounceFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+}
